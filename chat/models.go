eeed59b3466c6f0f0c68c9dff8cee2c5d33a2538
ashupednekar <ashutoshpednekar15@gmail.com>
2024-05-06 20:47:14 +0530

    feat: added db

---
+++ chat/models.go
@@ -0,0 +1,54 @@
+package chat
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func GetDb() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Could not connect to database: ", err)
+	}
+	return db
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&Users{}, &Groups{}, &Messages{})
+}
+
+type Users struct {
+	ID     int `gorm:"primaryKey"`
+	Name   string
+	IP     string
+	Online bool
+}
+
+type Groups struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Users []int `gorm:"type:json"`
+}
+
+type Messages struct {
+	ID     int `gorm:"primaryKey"`
+	FromID int
+	From   Users `gorm:"constraint:OnDelete:CASCADE;"`
+	ToID   int
+	To     Groups `gorm:"constraint:OnDelete:CASCADE;"`
+	Text   sql.NullString
+	File   sql.NullByte
+}
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/ashupednekar/gotcp/server"
+import (
+	"github.com/ashupednekar/tcpchat/chat"
+)
 
 func main() {
-	server := server.NewServer(":3000")
-	server.Start()
+	// server := server.NewServer(":3000")
+	// server.Start()
+	db := chat.GetDb()
+	chat.Migrate(db)
 }
