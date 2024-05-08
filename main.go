b63059aac5939d049b35ca8dcde1137ba13889f8
ashupednekar <ashutoshpednekar15@gmail.com>
2024-05-09 00:14:46 +0530

    feat: added excalidraw file

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/ashupednekar/gotcp/server"
 	"github.com/ashupednekar/tcpchat/chat"
 )
 
 func main() {
-	// server := server.NewServer(":3000")
-	// server.Start()
 	db := chat.GetDb()
 	chat.Migrate(db)
+	server := server.NewServer(":3000")
+	server.Start()
 }
