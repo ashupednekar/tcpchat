ba36720dd6eb90f29a1d37c096e1d798e2e6afc8
ashupednekar <ashutoshpednekar15@gmail.com>
2024-05-09 01:48:48 +0530

    refactor: extracted handlers into HandleJoin and HandleChat

--- server/handler.go
+++ server/handler.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ashupednekar/tcpchat/chat/data_adapters/mutators"
 	"github.com/ashupednekar/tcpchat/chat/data_adapters/selectors"
+	"gorm.io/gorm"
 )
 
 func (s *Server) HandleConn(conn net.Conn) {
@@ -24,23 +25,31 @@ func (s *Server) HandleConn(conn net.Conn) {
 
 		switch {
 		case strings.HasPrefix(string(msg), "root:join"):
-			l := strings.Split(string(msg), ":")
-			Name := l[len(l)-2]
-			fmt.Printf("New user joining: |%s| ", Name)
-			err := mutators.CreateUser(s.Db, Name, conn.RemoteAddr().String())
-			if err != nil {
-				fmt.Fprintf(conn, "error creating user: ", err)
-			}
+			HandleJoin(s.Db, string(msg), conn)
 		case strings.HasPrefix(string(msg), "chat:"):
-			err, user := selectors.GetUser(s.Db, conn.RemoteAddr().String())
-			if err != nil {
-				fmt.Fprintf(conn, "error retrieving user: ", err)
-			}
-			fmt.Printf("received message from user: %s => %s\n", user.Name, string(msg))
-			l := strings.Split(string(msg), ":")
-			mutators.SaveTextMessage(s.Db, conn.RemoteAddr().String(), l[1], l[len(l)-2])
+			HandleChat(s.Db, string(msg), conn)
 		default:
 			fmt.Fprintf(conn, "invalid payload received, skipping")
 		}
 	}
+}
+
+func HandleJoin(db *gorm.DB, msg string, conn net.Conn) {
+	l := strings.Split(string(msg), ":")
+	Name := l[len(l)-2]
+	fmt.Printf("New user joining: |%s| ", Name)
+	err := mutators.CreateUser(db, Name, conn.RemoteAddr().String())
+	if err != nil {
+		fmt.Fprintf(conn, "error creating user: ", err)
+	}
+}
+
+func HandleChat(db *gorm.DB, msg string, conn net.Conn) {
+	err, user := selectors.GetUser(db, conn.RemoteAddr().String())
+	if err != nil {
+		fmt.Fprintf(conn, "error retrieving user: ", err)
+	}
+	fmt.Printf("received message from user: %s => %s\n", user.Name, string(msg))
+	l := strings.Split(string(msg), ":")
+	mutators.SaveTextMessage(db, conn.RemoteAddr().String(), l[1], l[len(l)-2])
 }
